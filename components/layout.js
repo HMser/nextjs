62c80e7494e9d752b2d650fac2333cd9edd74914
HM <95879255+HMser@users.noreply.github.com>
2022-12-21 19:16:46 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,10 @@
-import Navbar from './navbar'
+import Head from './Head'
 import Footer from './footer'
 
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar />
+      <Head />
       <main>{children}</main>
       <Footer />
     </>
