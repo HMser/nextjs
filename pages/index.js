492f96c74e82f788b64d67d55c910a80cd1f2d2f
HM <95879255+HMser@users.noreply.github.com>
2022-12-21 16:41:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,9 +28,9 @@ export default function Home() {
 
  </Head>
 
-  <Script src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/swiper-bundle.min.js'></Script>
-<Script src='https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.min.js'></Script>
-<Script  src="./script.js"></Script>
+  <Script strategy="lazyOnload" onLoad={onLoadFunc} src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/swiper-bundle.min.js'></Script>
+<Script strategy="lazyOnload" onLoad={onLoadFunc} src='https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.min.js'></Script>
+<Script strategy="lazyOnload" onLoad={onLoadFunc} src="./script.js"></Script>
       <>
   {/* partial:index.partial.html */}
   {/* HEADER */}
